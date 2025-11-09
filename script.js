fbd3b94eff4f26d050b03fd7a493aa95f050b809
Buage <103582788+Buage@users.noreply.github.com>
2025-11-09 18:54:47 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -809,27 +809,6 @@ document.getElementById('lightToggleInput').addEventListener('change', function(
     }
 })
 
-document.getElementById('submit-app-btn1').addEventListener('click', function() {
-    document.querySelector('.submit-app').style.display = 'flex'
-    document.querySelector('.overlay').style.display = 'block'
-});
-
-document.getElementById('submit-app-btn').addEventListener('click', function() {
-    document.getElementById('submit-app-btn').classList.add('submit-app-loading')
-    document.getElementById('submit-app-btn').textContent = "Submitting..."
-    document.getElementById('submit-app-btn').disabled = true
-
-    fetch("https://api.selfhostlist.org/submitApp.php?app=" + encodeURIComponent(document.getElementById('missingAppSubmitName').value))
-    .then(() => {
-        document.querySelector('.submit-app').style.display = 'none';
-        document.querySelector('.overlay').style.display = 'none';
-
-        document.getElementById('submit-app-btn').classList.remove('submit-app-loading')
-        document.getElementById('submit-app-btn').textContent = "Send"
-        document.getElementById('submit-app-btn').disabled = false
-    })
-});
-
 if (getCookie('light') === 'true') {
     document.body.classList.remove('dark')
     document.body.classList.add('light')
